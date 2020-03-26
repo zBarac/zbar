ca5f5361035b4308667023831b32437de7682202
mbrkic00 <62607962+mbrkic00@users.noreply.github.com>
2020-03-26 16:59:47 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             Console.Write("nešto");
             int a = 4;
             Console.WriteLine("Još nešto");
+            Console.WriteLine("Napravljrna je promjena");
         }
     }
 }
